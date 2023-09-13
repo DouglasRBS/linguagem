382d82df56836bdca7f08f3eb7dd04d8b3055d72
Douglas Ribowski <143445010+DouglasRBS@users.noreply.github.com>
2023-09-13 08:18:47 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int num1, num2;
+    printf("digite o primeiro número:");
+    scanf("%d",&num1);
+    
+    printf("digite o segudo numero:");
+    scanf("%d",&num2);
+    
+    if (num1 == num2); {
+        printf("os numeros sao iguais.\n");
+    
+        printf("os numeros sao diferentes.\n");
+    
+    }
+    return 0;
+}
