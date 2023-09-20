923e6c64d8627d8582ba9b7b8e3cd7b908d9ff0a
Douglas Ribowski <143445010+DouglasRBS@users.noreply.github.com>
2023-09-20 08:19:50 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int i;
+    
+    printf("numeros pares entre 1 a 100:\n");
+    
+    for (i = 1; i <= 100; i++)
+    if(i)
+     printf("%d\n", i);
+     
+     return 0;
+    
+}
