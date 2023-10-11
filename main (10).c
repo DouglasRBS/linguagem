2f082c2a07dbfca3729bf7e20e051ce5fcd24c02
Douglas Ribowski <143445010+DouglasRBS@users.noreply.github.com>
2023-10-11 09:07:11 -0300

    Add files via upload

---
+++ main (10).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    char p1[30], p2[30];
+    printf("informe palavra 1:");
+    gets(p1);
+    printf("informe palavra 2:");
+    gets(p2);
+    if(strcmp (p1,p2));
+    printf("são iguais");
+    
+    return 0;
+ 
+}
